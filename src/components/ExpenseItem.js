b7ee02f8b08da3828fceb88072bb2b320e09cf74
Gerald Akinola <akinolagerald@gmail.com>
2022-09-12 05:05:23 -0700

    Styling the Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import './ExpenseItem.css'
 function ExpenseItem() {
     return <div className='expense-item'>
         <div className='expense-item_date'>September 10th 2022</div>
-        <h2>Car Insurance</h2>
+        <h2 className='expense-item h2'>Car Insurance</h2>
         <div className='expense-item_price'>$400.24</div>
     </div>
 }
