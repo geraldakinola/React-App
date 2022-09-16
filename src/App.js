cdb09ca0d963ef9411995442e7717a5c36a8e1cc
Gerald Akinola <akinolagerald@gmail.com>
2022-09-16 05:37:59 -0700

    props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 400.26, date: new Date(2021, 2, 28) },
+    { title: 'New TV', amount: 400.26, date: new Date(2021, 2, 28) },
+    { title: 'Toilet Paper', amount: 400.26, date: new Date(2021, 2, 28) },
+    { title: 'House Insurance', amount: 400.26, date: new Date(2021, 2, 28) },
+  ]
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title}></ExpenseItem>
       <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
