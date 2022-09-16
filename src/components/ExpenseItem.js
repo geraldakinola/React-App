d74f63cc93c0cc44bbc7a6d95742308a2e67275c
Gerald Akinola <akinolagerald@gmail.com>
2022-09-16 05:03:29 -0700

    component in React

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 400.24
     return <div className='expense-item'>
         <div className='expense-item_date'>September 10th 2022</div>
-        <h2 className='expense-item h2'>Car Insurance</h2>
+        <h2>{4 * 4}</h2>
         <div className='expense-item_price'>$400.24</div>
     </div>
 }
