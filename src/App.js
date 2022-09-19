8990b6a4b270cf69fea633f34a38893f8696c39a
Gerald Akinola <akinolagerald@gmail.com>
2022-09-19 08:57:49 -0700

    passing data via props

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    { title: 'Car Insurance', amount: 400.26, date: new Date(2021, 2, 28) },
-    { title: 'New TV', amount: 400.26, date: new Date(2021, 2, 28) },
-    { title: 'Toilet Paper', amount: 400.26, date: new Date(2021, 2, 28) },
-    { title: 'House Insurance', amount: 400.26, date: new Date(2021, 2, 28) },
+    { id: 'e1', title: 'Car Insurance', amount: 400.26, date: new Date(2021, 2, 28) },
+    { id: 'e2', title: 'New TV', amount: 400.26, date: new Date(2021, 2, 28) },
+    { id: 'e3', title: 'Toilet Paper', amount: 400.26, date: new Date(2021, 2, 28) },
+    { id: 'e4', title: 'House Insurance', amount: 400.26, date: new Date(2021, 2, 28) },
   ]
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem title={expenses[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expense[3].amount} date={expense[3].date}></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
