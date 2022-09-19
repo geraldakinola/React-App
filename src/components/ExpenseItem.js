7f279c360a2bf0317a5c5e673ab2005bc83354ef
Gerald Akinola <akinolagerald@gmail.com>
2022-09-19 04:47:04 -0700

    Props in React

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,13 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 400.24
-    return <div className='expense-item'>
-        <div>{expenseDate.toISOString()}</div>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item_price'>${expenseAmount}</div>
-    </div>
+function ExpenseItem(props) {
+    return (
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
+            <h2>{props.title}</h2>
+            <div className='expense-item_price'>${props.amount}</div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
