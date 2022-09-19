3e2f1f23176d57525c78505b252cdb33863ed59c
Gerald Akinola <akinolagerald@gmail.com>
2022-09-19 16:14:03 -0700

    JS in React

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>Year</div>
+                <div>Day</div>
+            </div>
             <h2>{props.title}</h2>
             <div className='expense-item_price'>${props.amount}</div>
         </div>
