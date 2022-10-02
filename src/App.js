a3b76caed71fc50ddb5e7cac4e894f6ec346ddbd
Gerald Akinola <akinolagerald@gmail.com>
2022-10-02 03:14:44 -0700

    react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseDate from './components/ExpenseDate';
+import React from 'react';
 
-import ExpenseItem from "./components/Expenses";
+
+import Expenses from './components/Expenses/Expense';
 
 
 function App() {
@@ -10,15 +11,23 @@ function App() {
     { id: 'e3', title: 'Toilet Paper', amount: 400.26, date: new Date(2021, 2, 25) },
     { id: 'e4', title: 'House Insurance', amount: 400.26, date: new Date(2021, 2, 21) },
   ]
+  // return (
+  //   <div>
+  //     <h2>Let's get started</h2>
+  //     <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+  //     <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+  //     <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+  //     <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+  //   </div>
+  // );
+
+
   return (
     <div>
-      <h2>Let's get started</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}/>
     </div>
-  );
+  )
 }
 
 export default App;
