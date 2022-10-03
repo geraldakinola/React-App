019bb3e9b136f74ae06f1a9cec5ed15988b00136
Gerald Akinola <akinolagerald@gmail.com>
2022-10-03 04:33:26 -0700

    Event Handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,14 +5,18 @@ import Card from '../UI/Card';
 
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log('Clicked!!!!');
+    };
+
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <h2>{props.title}</h2>
             <div className='expense-item_price'>${props.amount}</div>
-            <button onClick={() => {console.log('Clicked!')}}>Change Title</button>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
         
     );
