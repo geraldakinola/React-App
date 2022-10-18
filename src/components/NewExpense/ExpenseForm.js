2c0b7c315aaab30c3a61565c55fedd2ec6dd58c7
Gerald Akinola <akinolagerald@gmail.com>
2022-10-18 04:39:39 -0700

    using states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-import './ExpenseForm.css'; 
+import './ExpenseForm.css';
+import './NewExpense.css';
  
 
 const ExpenseForm = () => {
+
+  const titleChangeHandler = (event) => {
+    console.log ('event,target.value');
+  };
+  
     return <form>
        <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type= "text"/>
+          <input type= "text" onChange={titleChangeHandler}/>
           </div>
            <div className="new-expense__control">
           <label>Amount</label>
